61f995f43d4d5b04443ad04b014ab9f7646e273f
Jérémy Girard <oqaislenning@gmail.com>
2025-05-23 23:11:50 +0200

    feat: default svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
+<script>
+    import Button from "../components/Button.svelte";
+</script>
+
 <div class="flex flex-col min-h-screen justify-center items-center">
     <h1>LA GRAND MÈRE DE EMILIEN JE LA BOURLINGUE ON CD</h1>
     <img src="emilien.webp" alt="Emilien" />
+    <Button nom="Emilien Chalon" />
 </div>
