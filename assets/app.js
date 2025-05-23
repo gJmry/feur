587bb88c79f8b83ff1691fc4b67bc4347ca12101
Jérémy Girard <oqaislenning@gmail.com>
2025-05-23 11:17:45 +0200

    feat: update add webpack

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+import './styles/app.scss';
+
+// You can specify which plugins you need
+import { Tooltip, Toast, Popover } from 'bootstrap';
+
+// start the Stimulus application
+import './bootstrap';
