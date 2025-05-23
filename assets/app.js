44a0f38ce6a1ffb63ff03ef0fda0f1037ef0178e
Jérémy Girard <oqaislenning@gmail.com>
2025-05-23 22:19:12 +0200

    feat: remove all

--- assets/app.js
+++
@@ -1,7 +0,0 @@
-import './styles/app.scss';
-
-// You can specify which plugins you need
-import { Tooltip, Toast, Popover } from 'bootstrap';
-
-// start the Stimulus application
-import './bootstrap';
